e2de3c9112bbcded2ed377b714f0749c3cdd9e79
PC219-13\NTUT <joannahsuu@gmail.com>
2017-12-09 10:58:56 +0800

    text commit

--- src/Text.java
+++ src/Text.java
@@ -1,8 +1,15 @@
+import java.io.DataInputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
 
 public class Text {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Socket socket = null;
+		InputStream is = null;
+		OutputStream os = null;
+		DataInputStream dis = null;
 
 	}
 
